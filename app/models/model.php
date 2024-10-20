229907e9f698f7bee2a65415aed69b1c75ae5575
FranWFernandez <145360524+FranWFernandez@users.noreply.github.com>
2024-10-20 19:30:14 -0300

    cambios en los model

--- app/models/model.php
+++ app/models/model.php
@@ -3,10 +3,11 @@
 require_once './app/models/config.php';
 class DB{
     private $db;
+    
     public function __construct(){
-        $this->db= new PDO("mysql:host=". DB_HOST . ";dbname=". DB_NAME .";charset=" .DB_Charset,DB_USER, DB_PASS );
-     
+      $this->db= new PDO("mysql:host=". DB_HOST . ";dbname=". DB_NAME .";charset=" .DB_Charset,DB_USER, DB_PASS );
     }
+    
     public  function connect(){
       return $this->db;
     }
